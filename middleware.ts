f20a56d5c359012c77fc922bfed2faa9fd00c7ad
andy ramaroson <andyramaroson@gmail.com>
2025-08-31 15:20:28 +0200

    [fix] clean code

--- middleware.ts
+++ middleware.ts
@@ -11,7 +11,7 @@ export async function middleware(request: NextRequest) {
 
     if (session) {
       const url = new URL(request.url);
-      url.pathname = "/";
+      url.pathname = "/orgs";
       return NextResponse.redirect(url.toString());
     }
   }
