579bc7ffe8d8cef154a05908e876b1160ac80a56
andy ramaroson <andyramaroson@gmail.com>
2025-09-01 11:44:51 +0200

    [feature] add Pergolas and VerandasHero components with filtering functionality

---
+++ app/(layout)/pergolas/page.tsx
@@ -0,0 +1,13 @@
+import PergolasSection  from "@/features/pergolas/pergolas-component";
+import PergolasHero  from "@/features/pergolas/hero";
+// import { SectionDivider } from "@/features/landing/section-divider";
+
+export default function Pergolas() {
+  return (
+    <>
+      <PergolasHero />
+      {/* <SectionDivider /> */}
+      <PergolasSection />
+    </>
+  );
+}
