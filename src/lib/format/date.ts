c98cf1acae47c599886d053b8ba3a7ec70673535
andy ramaroson <andyramaroson@gmail.com>
2025-09-01 12:33:04 +0200

    [fix] error date

--- src/lib/format/date.ts
+++ src/lib/format/date.ts
@@ -1,6 +1,6 @@
 import { format } from "date-fns";
 import { enUS } from "date-fns/locale";
 
-export const formatDate = (date: Date, p0: string) => {
+export const formatDate = (date: Date, p0?: string) => {
   return format(date, "MMMM d, yyyy", { locale: enUS });
 };
