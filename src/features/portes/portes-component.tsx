0f0f87990b0ba73f5da60a537889d9bc6c0ac84e
andy ramaroson <andyramaroson@gmail.com>
2025-10-06 18:26:14 +0200

    [Feature] update seed 135 portes

--- src/features/portes/portes-component.tsx
+++ src/features/portes/portes-component.tsx
@@ -481,7 +481,7 @@ const PortesGrid = ({
   portes: Product[];
   onPorteClick: (porte: Product) => void;
 }) => (
-  <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+  <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
     {portes.map((porte, index) => (
       <PorteCard
         key={porte.id}
