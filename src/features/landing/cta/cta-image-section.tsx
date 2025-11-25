0ae0096cb26f56319434a130bbf0933c857ee20b
andy ramaroson <andyramaroson@gmail.com>
2025-11-25 14:48:33 +0100

    [fix] clean code cta-section-image

--- src/features/landing/cta/cta-image-section.tsx
+++ src/features/landing/cta/cta-image-section.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import { Typography } from "@/components/nowts/typography";
-import { buttonVariants } from "@/components/ui/button"; 
 import { SectionLayout } from "../section-layout";
 import { useSession } from "@/lib/auth-client";
 import QuoteRequestModule from "@/features/landing/quote-request-module"; 
 
 export const CTAImageSection = () => {
-  const { data: session } = useSession();
+  // const { data: session } = useSession();
   // const imageUrl = "https://images.unsplash.com/photo-1549419163-9efd3a11f264?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
   
   return (
@@ -34,9 +33,7 @@ export const CTAImageSection = () => {
         <Typography className="text-center text-lg font-medium max-w-2xl">
           Remplissez ce formulaire pour planifier notre premier échange et définir les contours de votre futur projet.
         </Typography>
-        <QuoteRequestModule 
-          className="mt-4 text-gray-900 font-bold bg-white hover:bg-white/90 shadow-xl transition-transform hover:scale-[1.02] px-8 py-4 rounded-lg text-lg"
-        />
+        <QuoteRequestModule />
       </SectionLayout>
     </div>
   );
